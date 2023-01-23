e73647dc5ec774ecb0afbe107573c574a6d648c1
Martin van Harmelen <martin.van.harmelen@triply.cc>
2023-01-23 10:47:55 +0545

    Make sequential for readable flamegraph

--- src/main.rs
+++ src/main.rs
@@ -35,10 +35,11 @@ fn main() -> Result<(), io::Error> {
     let mut counts = WalkDir::new(path)
         .into_iter()
         .collect::<Vec<_>>() // get all files
-        .into_par_iter()
+        // .into_par_iter()
+        .into_iter()
         .map(|x| x.unwrap().into_path())
         .filter(|p| (!p.is_dir()))
-        .try_fold(new_hash_map, |mut counts, path| {
+        .try_fold(new_hash_map(), |mut counts, path| {
             let mut map: HashMap<_, usize> = new_hash_map();
             let contents = read_to_string(path)?;
 
@@ -52,14 +53,14 @@ fn main() -> Result<(), io::Error> {
                 *df += 1;
             });
             Ok::<_, io::Error>(counts)
-        })
-        .try_reduce(new_hash_map, |mut left_counts, right_counts| {
-            right_counts.into_iter().for_each(|(token, (tf, df))| {
-                let (left_tf, left_df) = left_counts.entry(token).or_default();
-                *left_tf += tf;
-                *left_df += df;
-            });
-            Ok(left_counts)
+        // })
+        // .try_reduce(new_hash_map, |mut left_counts, right_counts| {
+        //     right_counts.into_iter().for_each(|(token, (tf, df))| {
+        //         let (left_tf, left_df) = left_counts.entry(token).or_default();
+        //         *left_tf += tf;
+        //         *left_df += df;
+        //     });
+        //     Ok(left_counts)
         })?;
 
     if args.min_frequency > 0 {
